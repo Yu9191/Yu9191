1188df35f2f6357b86d602718cc7985554a06deb
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-22 02:16:39 +0800

    Update nicegram.js

--- nicegram.js
+++ nicegram.js
@@ -1,21 +1,23 @@
-//quantumultx
-//@ios151,测试完及时删除
+/*
+#Quantumultx
+#@ios151,测试完及时删除
 
 
 
 
 [rewrite_local]
 ^https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\w+$ url script-response-body https://raw.githubusercontent.com/Yu9191/Yu9191/main/nicegram.js
-//测试完及时删除
+
 [MITM]
 hostname = restore-access.indream.app
-// Nicegram
+
+*/
 const vip1 = '/restoreAccess';
 
-// 处理请求并修改响应
+
 function handleRequest(request) {
   if (request.url.indexOf(vip1) !== -1) {
-    // 返回VIP会员数据
+    
     const responseData = {
       "code": 1,
       "body": {
@@ -29,7 +31,6 @@ function handleRequest(request) {
     return { response: { body: JSON.stringify(responseData) } };
   }
 
-  // 对于其他请求，保持原始响应
   return { response: request.response };
 }
 
