d4cf63fcc4f7ade362ea242aa313cdeba9a40577
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-22 01:01:01 +0800

    Update cad2.js

--- cad2.js
+++ cad2.js
@@ -1,5 +1,12 @@
-// 模拟请求的数据
-var requestData = {
+var body = $response.body;
+var baby = JSON.parse(body);
+const vip1 = '/account';
+const vip2 = '/alipay/auth';
+const vip3 = '/authorize/query';
+
+if ($request.url.indexOf(vip1) != -1){
+  // 模拟请求的数据
+  var requestData = {
   "data": {
     "platformNumber": "1",
     "appId": "1108096074",
@@ -7,9 +14,8 @@ var requestData = {
   },
   "code": 0
 };
-
-// 模拟返回的数据
-var responseData = {
+  // 模拟返回的数据
+  var responseData = {
   "body": {
     "cadToken": "761b03f5-c01e-4e66-97dc-cc1fd2011d4f",
     "loginIdentity": "1689926495150",
@@ -28,16 +34,31 @@ var responseData = {
   },
   "code": 1
 };
-
-// 模拟请求处理
-function handleRequest(requestData) {
+  // 模拟请求处理
+  function handleRequest(requestData) {
   // 这里可以对requestData进行处理，根据不同的请求参数返回不同的responseData
   // 在实际测试中，您可以根据您的需求修改模拟的请求处理逻辑
   return responseData;
+};
+  // 调用handleRequest处理请求并返回模拟数据
+  var response = handleRequest(requestData);
+  body = JSON.stringify(response);
 }
 
-// 调用handleRequest处理请求并返回模拟数据
-var response = handleRequest(requestData);
+if ($request.url.indexOf(vip2) != -1){
+  baby = {
+  "endDate": "5201-03-14 19:57:57",
+  "code": "1"
+};
+  body = JSON.stringify(baby);
+}
 
-// 返回修改后的数据
-$done({ body: JSON.stringify(response) });
+if ($request.url.indexOf(vip3) != -1){
+  baby = {
+  "endDate": "5201-03-14 19:57:57",
+  "code": "1"
+};
+  body = JSON.stringify(baby);
+}
+
+$done({body});
